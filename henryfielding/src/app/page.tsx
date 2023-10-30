c8d7b12eb08859c39309350bb16aaad5995b45f6
developmentHF <development.henry@gmail.com>
2023-10-30 18:44:33 +1100

    break down HeaderText content into individual characters for animation purposes

--- henryfielding/src/app/page.tsx
+++ henryfielding/src/app/page.tsx
@@ -2,7 +2,7 @@ import HeaderText from "../components/HeaderText";
 export default function Home() {
   return (
     <div className="flex w-screen items-center justify-center mt-52">
-        <HeaderText content="Henry Fielding"/>
+        <HeaderText firstName="Henry" lastName="Fielding"/>
     </div>
   )
 }
--- henryfielding/src/components/HeaderText.tsx
+++ henryfielding/src/components/HeaderText.tsx
@@ -1,9 +1,21 @@
 interface FullName {
-    content: string;
+    firstName: string,
+    lastName: string,
 }
 
 export default function HeaderText(props:FullName) {
+    const firstNameChars : string[] = props.firstName.split('');
+    const lastNameChars : string[] = props.lastName.split('');
+
     return (
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">{props.content}</h1>
+        <>
+            {firstNameChars.map((char : string, index : number) => (
+                <span className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text8xl" key={index}>{char}</span>
+            ))}
+            <span className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text8xl">&nbsp;</span> {/* This is a space */}
+            {lastNameChars.map((char : string, index : number) => (
+                <span className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text8xl" key={index}>{char}</span>
+            ))}
+        </>
     );
 }
