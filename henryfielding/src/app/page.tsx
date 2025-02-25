30d92bd739c3f086cfb4c25d865697a55c523941
Henry Fielding <hfielding@student.unimelb.edu.au>
2025-02-25 14:16:49 +1100

    Add basic experience props

--- henryfielding/src/app/page.tsx
+++ henryfielding/src/app/page.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import HeaderText from "../components/HeaderText";
 import Summary from "@/components/cv/Summary";
 import ScrollArrow from "../components/ScrollArrow"; 
+import Experience from "@/components/cv/Experience";
 
 export default function Home() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -21,20 +22,31 @@ export default function Home() {
   }, []);
 
   return (
-    <>
-      <div className="flex justify-center items-center min-h-screen px-4 relative bg-[#0064E6]">
-        <div className="max-w-2xl text-[#C3F6FF] text-center">
-          <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-serif mb-6 font-bold">
-            henry
-          </h1>
-          <Summary paragraph="software developer & masters student" />
+    <div className="">
+        <div className="flex items-center justify-center min-h-screen px-4 relative">
+          <div className="max-w-2xl text-[#C3F6FF] text-center">
+            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-serif mb-6 font-bold">
+              henry
+            </h1>
+            <Summary paragraph="software developer & masters student" />
+          </div>
+
+          <ScrollArrow isScrolled={isScrolled} />
         </div>
 
-        <ScrollArrow isScrolled={isScrolled} />
-      </div>
+        <div className="flex justify-start items-center px-4 relative">
+          <div className="flex justify-start">
+            <Experience
+                jobTitle="Software Developer"
+                company="Jocapps GmbH"
+                jobStartYear="2020"
+                jobEndYear="Present"
+                jobBulletPoints={["Developed web applications", "Collaborated with teams", "Implemented features"]}
+            />
+          </div>
+        </div>
 
-      <p className="p-4">here is some text</p>
-      <div className="h-screen"></div> {/* Extra space to scroll */}
-    </>
+        <div className="h-40"></div> {/* Extra space to scroll */}
+    </div>
   );
 }
