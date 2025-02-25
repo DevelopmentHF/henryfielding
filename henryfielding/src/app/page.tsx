1b7f279721333c1377db5a53d099cf3fd5510851
Henry Fielding <hfielding@student.unimelb.edu.au>
2025-02-25 14:02:37 +1100

    Setup general structure of new styling

--- henryfielding/src/app/page.tsx
+++ henryfielding/src/app/page.tsx
@@ -1,57 +1,40 @@
+'use client'
+import { useState, useEffect } from "react";
 import HeaderText from "../components/HeaderText";
+import Summary from "@/components/cv/Summary";
+import ScrollArrow from "../components/ScrollArrow"; 
 
 export default function Home() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="flex flex-col w-screen items-start justify-center m-2 gap-4">
-      <h1 className="text-gray-400">&lt;h1&gt;</h1>
-    	<h1 className="text-gray-400 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl"> const char* name = </h1> 
-      <div className="flex ">
-		<HeaderText firstName='"Henry' lastName='Fielding"'/>
-      </div>
-      <h1 className="text-gray-400">&lt;/h1&gt;</h1>
-
-      <h1 className="text-gray-400">&lt;p&gt;</h1>
-      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
-        software developer @ <span className="italic">jocapps</span> <span className="font-bold">&amp;&amp;</span> master of electrical eng @ <span className="italic">unimelb</span>
-      </h1>
-      <h1 className="text-gray-400">&lt;/p&gt;</h1>
-
-      <h1 className="text-gray-400">&lt;h2&gt;</h1>
-      <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">my tech stack == </h2>
-      <h1 className="text-gray-400">&lt;/h2&gt;</h1>
-
-      <h1 className="text-gray-400">&lt;ul&gt;</h1>
-      <div className="flex flex-wrap gap-2">
-        <div>
-          <span className="text-gray-400">&lt;li&gt;</span>
-          <span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">java</span>
-          <span className="text-gray-400">&lt;/li&gt;</span>
+    <>
+      <div className="flex justify-center items-center min-h-screen px-4 relative bg-[#0064E6]">
+        <div className="max-w-2xl text-[#C3F6FF] text-center">
+          <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-serif mb-6 font-bold">
+            henry
+          </h1>
+          <Summary paragraph="software developer & masters student" />
         </div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">c</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">python</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">javascript</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">react</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">java</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">lua</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">swift</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">pandas</span><span className="text-gray-400">&lt;/li&gt;</span></div>
-        <div><span className="text-gray-400">&lt;li&gt;</span><span className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">sql</span><span className="text-gray-400">&lt;/li&gt;</span></div>
+
+        <ScrollArrow isScrolled={isScrolled} />
       </div>
-      <h1 className="text-gray-400">&lt;/ul&gt;</h1>
 
-      <div className="flex flex-wrap items-center">
-        <h1 className="text-gray-400">&lt;a&gt;</h1>
-        <a href="https://github.com/developmentHF" className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl bg-gray-200 rounded-xl shadow-xl p-4 hover:bg-white hover:text-gray-400">github</a>
-        <h1 className="text-gray-400">&lt;/a&gt;</h1>
-
-        <h1 className="text-gray-400">&lt;a&gt;</h1>
-        <a href="https://www.linkedin.com/in/henryfielding-aus/" className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl bg-gray-200 rounded-xl shadow-xl p-4 hover:bg-white hover:text-gray-400">linkedin</a>
-        <h1 className="text-gray-400">&lt;/a&gt;</h1>
-
-        <h1 className="text-gray-400">&lt;a&gt;</h1>
-        <a href="https://developmenthf.itch.io/" className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl bg-gray-200 rounded-xl shadow-xl p-4 hover:bg-white hover:text-gray-400">itch.io</a>
-        <h1 className="text-gray-400">&lt;/a&gt;</h1>
-      </div>
-    </div>
+      <p className="p-4">here is some text</p>
+      <div className="h-screen"></div> {/* Extra space to scroll */}
+    </>
   );
 }
