9d009c579ca401aaa1df6373e365b22fd1f745ca
Henry Fielding <hfielding@student.unimelb.edu.au>
2025-02-25 14:22:58 +1100

    Create Hero component

--- henryfielding/src/app/page.tsx
+++ henryfielding/src/app/page.tsx
@@ -1,51 +1,34 @@
-'use client'
-import { useState, useEffect } from "react";
-import HeaderText from "../components/HeaderText";
-import Summary from "@/components/cv/Summary";
-import ScrollArrow from "../components/ScrollArrow"; 
+import Hero from "../components/Hero";
 import Experience from "@/components/cv/Experience";
 
 export default function Home() {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 10) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="">
-        <div className="flex items-center justify-center min-h-screen px-4 relative">
-          <div className="max-w-2xl text-[#C3F6FF] text-center">
-            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-serif mb-6 font-bold">
-              henry
-            </h1>
-            <Summary paragraph="software developer & masters student" />
-          </div>
+        <Hero name="henry" summaryText="software developer & masters student"></Hero>
 
-          <ScrollArrow isScrolled={isScrolled} />
-        </div>
+        <Experience
+            jobTitle="Software Developer"
+            company="Jocapps GmbH"
+            jobStartYear="2020"
+            jobEndYear="Present"
+            jobBulletPoints={["Developed web applications", "Collaborated with teams", "Implemented features"]}
+        />
+        <Experience
+            jobTitle="Software Developer"
+            company="Jocapps GmbH"
+            jobStartYear="2020"
+            jobEndYear="Present"
+            jobBulletPoints={["Developed web applications", "Collaborated with teams", "Implemented features"]}
+        />
+        <Experience
+            jobTitle="Software Developer"
+            company="Jocapps GmbH"
+            jobStartYear="2020"
+            jobEndYear="Present"
+            jobBulletPoints={["Developed web applications", "Collaborated with teams", "Implemented features"]}
+        />
 
-        <div className="flex justify-start items-center px-4 relative">
-          <div className="flex justify-start">
-            <Experience
-                jobTitle="Software Developer"
-                company="Jocapps GmbH"
-                jobStartYear="2020"
-                jobEndYear="Present"
-                jobBulletPoints={["Developed web applications", "Collaborated with teams", "Implemented features"]}
-            />
-          </div>
-        </div>
-
+        
         <div className="h-40"></div> {/* Extra space to scroll */}
     </div>
   );
---
+++ henryfielding/src/components/Hero.tsx
@@ -0,0 +1,43 @@
+'use client'
+import { useState } from "react";
+import { useEffect } from "react";
+import ScrollArrow from "../components/ScrollArrow"; 
+import Summary from "./cv/Summary";
+
+interface heroProps {
+    name: string;
+    summaryText: string;
+}
+
+export default function Hero( props: heroProps ) {
+
+    const [isScrolled, setIsScrolled] = useState(false);
+    
+    useEffect(() => {
+    const handleScroll = () => {
+        if (window.scrollY > 10) {
+        setIsScrolled(true);
+        } else {
+        setIsScrolled(false);
+        }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+
+    return (
+        <div className="flex items-center justify-center min-h-screen px-4 relative">
+          <div className="max-w-2xl text-[#C3F6FF] text-center">
+            <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-serif mb-6 font-bold">
+              henry
+            </h1>
+            <Summary paragraph={props.summaryText} />
+          </div>
+
+          <ScrollArrow isScrolled={isScrolled} />
+        </div>
+    );
+}
+
